677d0845a104f18f2d2de3abdffca2c097885b18
nishihara <nobuo0125211@gmail.com>
2018-08-02 16:41:09 +0300

    add background color change button

--- wear/src/main/java/com/example/eiji/disitalwristband/MainActivity.java
+++ wear/src/main/java/com/example/eiji/disitalwristband/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.eiji.disitalwristband;
 
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final Random RANDOM = new Random();
+    public View root;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        root = findViewById(R.id.root);
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int red = RANDOM.nextInt(255);
+                int blue = RANDOM.nextInt(255);
+                int green = RANDOM.nextInt(255);
+
+                int color = Color.rgb(red, green, blue);
+                root.setBackgroundColor(color);
+            }
+        });
     }
 }
