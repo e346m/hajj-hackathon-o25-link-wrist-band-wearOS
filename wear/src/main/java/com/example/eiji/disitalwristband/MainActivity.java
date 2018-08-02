1a51630ea26bad2eb0245cb684e8d25eb8725cbd
eiji mishiro <eiji346g@gmail.com>
2018-08-02 17:33:41 +0900

    add MainActivity

---
+++ wear/src/main/java/com/example/eiji/disitalwristband/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eiji.disitalwristband;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
