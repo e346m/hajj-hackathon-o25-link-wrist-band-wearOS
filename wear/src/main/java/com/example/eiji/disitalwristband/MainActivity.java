cdbc1c41ae209e69bb71fda795e4ead4b3b75e8b
eiji mishiro <eiji346g@gmail.com>
2018-08-03 10:46:22 +0300

    finishgit add -A

--- wear/src/main/java/com/example/eiji/disitalwristband/MainActivity.java
+++ wear/src/main/java/com/example/eiji/disitalwristband/MainActivity.java
@@ -1,25 +1,15 @@
 package com.example.eiji.disitalwristband;
 
-import android.animation.ValueAnimator;
-import android.graphics.Color;
-import android.graphics.drawable.TransitionDrawable;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 
@@ -29,11 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     public static final Random RANDOM = new Random();
     public View root;
-    public Button button;
-    public ValueAnimator anim;
-
-    public Handler mHandler = new Handler();
-    public Runnable restartAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,76 +27,20 @@ public class MainActivity extends AppCompatActivity {
         MyFirebaseInstanceIDService instance = new MyFirebaseInstanceIDService();
         instance.onTokenRefresh();
         setContentView(R.layout.activity_main);
-        root = findViewById(R.id.root);
-        button = (Button) findViewById(R.id.button1);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(
+                mMessageReceiver, new IntentFilter("color"));
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                boolean flag = RANDOM.nextBoolean();
-                if(flag) {
-                    anim.end();
-                    int red = RANDOM.nextInt(255);
-                    int blue = RANDOM.nextInt(255);
-                    int green = RANDOM.nextInt(255);
-
-                    int color = Color.rgb(red, green, blue);
-                    root.setBackgroundColor(color);
-                    button.setText("Yes, they already departured. Their wrist band color is the same as the background color.");
-                    button.setEnabled(false);
-                    restartAnimation = new Runnable() {
-                        public void run() {
-                            startAnimation(anim);
-                            mHandler.removeCallbacks(restartAnimation);
-                            mHandler.postDelayed(restartAnimation, 5000);
-                        }
-                    };
-
-                    mHandler.postDelayed(restartAnimation, 5000);
-                }else{
-                    button.setText("They are still around here. Please look around to find them");
-                }
-            }
-        });
-        anim = ValueAnimator.ofFloat(0, 1);
-        startAnimation(anim);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                int red = RANDOM.nextInt(255);
-                int blue = RANDOM.nextInt(255);
-                int green = RANDOM.nextInt(255);
-
-                int color = Color.rgb(red, green, blue);
-                root.setBackgroundColor(color);
-            }
-        });
     }
 
-      public void startAnimation(ValueAnimator anim){
-          anim.setDuration(2000);
-
-          final float[] hsv;
-          int runColor;
-          int hue = 0;
-          hsv = new float[3]; // Transition color
-          hsv[1] = 1;
-          hsv[2] = 1;
-          anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-              @Override
-              public void onAnimationUpdate(ValueAnimator animation) {
-
-                  hsv[0] = 360 * animation.getAnimatedFraction();
-                  root.setBackgroundColor(Color.HSVToColor(hsv));
-              }
-          });
-
-          anim.setRepeatCount(Animation.INFINITE);
-
-          button.setText("check if your group already started to move");
-          button.setEnabled(true);
-
-          anim.start();
-      }
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("color");
+            Log.d("color2",  message);
+            root = findViewById(R.id.root);
+            int color = Color.parseColor(message);
+            root.setBackgroundColor(color);
+           ;
+        }
+    };
 }
--- wear/src/main/java/com/example/eiji/disitalwristband/MyFirebaseMessagingService.java
+++ wear/src/main/java/com/example/eiji/disitalwristband/MyFirebaseMessagingService.java
@@ -1,6 +1,9 @@
 package com.example.eiji.disitalwristband;
 
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,6 +18,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
+    public void sendMessageToActivity(String msg) {
+        Intent intent = new Intent("color"); // You can also include some extra data.
+        Log.d("color1", msg);
+        intent.putExtra("color", msg);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
@@ -24,14 +34,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            //Log.d(TAG, "Message data payload: " + remoteMessage.getData().values().toArray()[0]);
+           String text = (String) remoteMessage.getData().values().toArray()[0];
+           sendMessageToActivity(text);
 
         }
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
     }
     /**
      * Called if InstanceID token is updated. This may occur if the security of
